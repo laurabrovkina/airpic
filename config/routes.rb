0e4277f1340cf9736a724e511bff963064045676
laurabrovkina <laurabrovkina@mail.ru>
2018-02-07 21:56:11 +1300

    07022018

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+
+  devise_for :users,
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+              path: '',
+              path_name: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+              controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+
+  resources :users, only: [:show]
+  resources :rooms, except: [:edit] do
+    member do
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+    end
+  end
+end
